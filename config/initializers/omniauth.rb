14c3b2334d0b37c2fa93649865dd0712f650655a
Jakub Misterka <san.y4ku@gmail.com>
2011-12-27 16:25:28 -0600

    full host change for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.full_host = "http://localhost:3000"
+#OmniAuth.config.full_host = "http://localhost:3000"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['TEST_KEY'], ENV['TEST_SECRET'], { :scope => 'userinfo.email,userinfo.profile,https://www.googleapis.com/auth/calendar' } #'https://www.google.com/calendar/feeds/' }
